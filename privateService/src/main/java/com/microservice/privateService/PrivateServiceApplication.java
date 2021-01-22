a5fb30ce600e60f39ab5c25f89f00ab8e9da954e
SubratMoharana <subrat.moharana@tarento.com>
2021-01-22 17:23:11 +0530

    new project

---
+++ privateService/src/main/java/com/microservice/privateService/PrivateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.privateService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateServiceApplication.class, args);
+	}
+
+}
